b9ea826c6d895d95cf71011aa247d71640bc97ac
saidan-hasbianto <saidan.hasbianto@gmail.com>
2022-10-06 22:17:32 +0700

    add data to localstorage

---
+++ src/app/models/employee.ts
@@ -0,0 +1,12 @@
+export class Employee {
+    id: string;
+    username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    birthDate: Date;
+    basicSalary: number;
+    status: string;
+    group: string;
+    description: string;
+}
--- src/app/pages/employee/employee-list/employee-list.component.ts
+++ src/app/pages/employee/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
 import { SmartTableData } from '../../../@core/data/smart-table';
+import { Employee } from '../../../models/employee';
 
 @Component({
   selector: 'ngx-employee-list',
@@ -17,6 +18,41 @@ export class EmployeeListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.createFakeData();
+
+    console.log(localStorage.getItem("employeeList"));
+    console.log(localStorage);
+
   }
 
+
+  createFakeData() {
+    let a = new Employee();
+    a.id = "d5024df9-fee1-497d-9c7d-b56a43cc18a6";
+    a.basicSalary = 4300000;
+    a.birthDate = new Date('1988-01-27');
+    a.description = "tester1";
+    a.email = "tester1@nanana.com";
+    a.firstName = "tester1";
+    a.group = "test";
+    a.lastName = "staff";
+    a.status = "test";
+    a.username = "tester1";
+    let employeeList = [];
+    employeeList.push(a);
+    a = new Employee();
+    a.id = "f1465bca-38b3-471d-b796-a73985dfb706";
+    a.basicSalary = 5000000;
+    a.birthDate = new Date('1988-01-27');
+    a.description = "tester2";
+    a.email = "tester2@nanana.com";
+    a.firstName = "tester2";
+    a.group = "tester2";
+    a.lastName = "staff";
+    a.status = "tester2";
+    a.username = "tester2";
+    employeeList.push(a);
+
+    localStorage.setItem("employeeList", JSON.stringify(employeeList));
+  }
 }

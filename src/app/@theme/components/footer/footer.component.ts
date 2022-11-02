cffb31e89655054a669ca529cdc3941ee4c47bff
saidan-hasbianto <saidan.hasbianto@gmail.com>
2022-11-02 21:52:24 +0700

    clean code

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -6,14 +6,7 @@ import { Component } from '@angular/core';
   template: `
     <span class="created-by">
       Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
-    </span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
-  `,
+    </span>`,
 })
 export class FooterComponent {
 }
